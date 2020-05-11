b849c6e476fb5986b6122a5e2a3008dcadcd72b6
Vincent Ma <vincent.ma810@gmail.com>
2020-05-11 19:29:57 -0400

    Send API call

--- index.js
+++ index.js
@@ -86,5 +86,16 @@ function callSendAPI(sender_psid, response) {
     "message": response
   }
 
-  
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
 }
