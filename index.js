711872ed899d4d5bc963b1c40a4e8dfdbf73abed
Vincent Ma <vincent.ma810@gmail.com>
2020-05-27 16:58:09 -0600

    Remove messenger code

--- index.js
+++ index.js
@@ -4,106 +4,103 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const app = express().use(bodyParser.json());
+const fs = require("fs");
+const {google} = require('googleapis');
 
 app.listen(process.env.PORT || 1337, () => console.log("webhook listening"));
 
-// Creates the endpoint for our webhook 
-app.post('/webhook', (req, res) => {  
- 
-    let body = req.body;
+app.get("/", (req, res) => {
+  res.status(200).send("blank").end();
+});
 
-    console.log(body);
+// Endpoint for Google Drive push notifications
+app.post("/notifications", (req, res) => {
 
-    // Checks this is an event from a page subscription
-    if (body.object === 'page') {
-  
-      // Iterates over each entry - there may be multiple if batched
-      body.entry.forEach(function(entry) {
-  
-        // Gets the message. entry.messaging is an array, but 
-        // will only ever contain one message, so we get index 0
-        let webhook_event = entry.messaging[0];
-        let sender_psid = webhook_event.sender.id;
-        console.log(webhook_event);
+  console.log("push notification received");
+});
 
-        if (webhook_event.message) {
-          handlemsg(sender_psid, webhook_event.message);
-        } else if (webhook_event.postback) {
-          handlePostback(sender_psid, webhook_event.postback);
+app.get("/notifications", (req, res) => {
+  res.status(200).send("notif").end();
+});
+
+// drive auth functions
+async function authorize(credentials) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  oAuth2Client.on("tokens", (token) => {
+    if (token.refresh_token) {
+      fs.writeFile("token.json", JSON.stringify(token), (err) => {
+        if (err) {
+          console.log("error writing token");
         }
+      })
+      // console.log(token);
+    }
+    // console.log(token);
+    postreq(oAuth2Client, token);
+  });
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getAccessToken(oAuth2Client, postreq);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    driveWatchRequest(oAuth2Client);
+  });
+}
+
+function getAccessToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error retrieving access token', err);
+      oAuth2Client.setCredentials(token);
+      console.log("Hello");
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
       });
-  
-      // Returns a '200 OK' response to all requests
-      res.status(200).send('EVENT_RECEIVED');
-    } else {
-      // Returns a '404 Not Found' if event is not from a page subscription
-      res.sendStatus(404);
-    }
-  
-});
-
-app.get("/webhook", (req, res) => {
-    let VERIFY_TOKEN = "yurikek";
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-
-    if (mode && token) {
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-            console.log("WEBHOOK VERIFIED");
-            res.status(200).send(challenge);
-        } else {
-            res.status(403);
-        }
-    }
-});
-
-// handle message events
-function handlemsg(sender_psid, received_message) {
-  let response;
-
-  if (received_message.text) {
-    response = { "text" : `well "${received_message.text}" to you too`};
-  } else {
-    response = {"text" : "you sent me nothing you git"};
-  }
-
-  
-  callSendAPI(sender_psid, response);
+      callback(oAuth2Client, token);
+    });
+  });
 }
 
-// handle message postback events
-function handlePostback(sender_psid, received_postback) {
-  // not really a thing for what i wanna do i think
-}
-
-// send response
-function callSendAPI(sender_psid, response) {
-  let request_body = {
-    "recipient": {
-      "id" : sender_psid
-    },
-    "message": response
+async function driveWatchRequest(client) {
+  const {token} = await client.getAccessToken();
+  var reqbody = { 
+      "id": "yurikek",
+      "type": "web_hook",
+      "address": "https://yurupdate.wm.r.appspot.com/notifications"
   }
-
   request({
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-    "method": "POST",
-    "json": request_body
+      url: "https://www.googleapis.com/drive/v3/files/18xVLgwhixugbfBVQBO_67aRre4vrr4mAJMAV-y4PCJ8/watch", 
+      method: "POST",
+      headers: {
+          "Authorization": "Bearer " + token,
+          "Content-Type": "application/json"
+      },
+      json: reqbody
   }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!')
-    } else {
-      console.error("Unable to send message:" + err);
-    }
-  }); 
+      if (!err) {
+          // console.log(res);
+          console.log(body);
+      }
+      fs.writeFile("res.txt", res, (err) => {
+          if (err) {
+              console.log("error writing to file");
+          }
+      })
+  });
 }
-
-function multisend() {
-  var temppsid = 2932545733449046;
-  let response = {"text": "multisend message"};
-  callSendAPI(temppsid, response);
-}
-
-setInterval(multisend, 60000);
